dfd4992f9befb7aed732ae5b7dc3a90aafc01c04
terenceWin <anday312@gmail.com>
2025-10-19 16:53:39 +0900

    Initial Commit

---
+++ TipsyTests/TipsyTests.swift
@@ -0,0 +1,17 @@
+//
+//  TipsyTests.swift
+//  TipsyTests
+//
+//  Created by Terence Win on 2025/10/19.
+//
+
+import Testing
+@testable import Tipsy
+
+struct TipsyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
