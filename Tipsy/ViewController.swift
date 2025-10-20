9553e0ac1acca708e8a524c06e9b71c9f4797eeb
terenceWin <anday312@gmail.com>
2025-10-20 20:41:06 +0900

    UIDesign and TextField Functionality

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userInput: UITextField!
+    @IBOutlet weak var zeroPercentTip: UIButton!
+    @IBOutlet weak var tenPercentTip: UIButton!
+    @IBOutlet weak var twentyPercentTip: UIButton!
+    @IBOutlet weak var label: UILabel?
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var calculateButton: UIButton!
+    
+    var people : Int = 0
+    var tipPercentage: Double? = nil
+    var totalBill: Double? = nil
+    var input: String? = nil
+    var totalBillForEachPerson: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        userInput.addTarget(self, action: #selector(textFieldChange(_:)), for: .editingChanged)
 
+        let tap = UIGestureRecognizer(target: self, action: #selector(dismissTextField))
+        view.addGestureRecognizer(tap)
+        
+        people = Int(stepper.value)
+        label!.text = String(people)
+        
+        
+    }
+    
+    @objc func dismissTextField(){
+        view.endEditing(true)
+    }
+    
+    @IBAction func textFieldChange(_ sender: UITextField){
+        if let input = sender.text{
+            let doubleInput = Double(input)
+            totalBill = doubleInput
+        }else{
+            totalBill = 0.0
+        }
+    }
+    
+    @IBAction func tipSelected(_ sender: UIButton){
+        switch sender.currentTitle {
+            case "0%":
+                tipPercentage = 0.0
+            case "10%":
+                tipPercentage = 0.1
+            case "20%":
+                tipPercentage = 0.2
+            default:
+                break
+            }
+        print(tipPercentage!)
+    }
+    
 
 }
-
